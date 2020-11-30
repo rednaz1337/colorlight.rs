53435e5c09e84abaf5b6e222341c29bc3d586a91
Heinrich Zander <heinrich.zander@datenkunst.de>
2020-11-30 17:58:55 +0100

    moved led panel logic to 'panel.rs'

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,85 @@
 use std::io::ErrorKind::WouldBlock;
-use std::net::{SocketAddr, UdpSocket};
-use std::time::{Duration, Instant};
 
-const IP_ADDRESS: &str = "192.168.178.50";
+mod panel;
 
-const PANEL_WIDTH: usize = 64;
-const PANEL_HEIGHT: usize = 32;
+///divisor is the FPS
+const TARGET_TIME: u64 = 1000 / 30;
 
-const PORTS: [u16; 4] = [
-    26177, // top left
-    26184, // top right
-    26180, // bottom left
-    26192,
-]; // bottom right
-
-//const OFFSET_X: usize = 0;
-//const OFFSET_Y: usize = 0;
-
-const OFFSETS: [[usize; 2]; 4] = [[0, 0], [64, 0], [0, 32], [64, 32]];
 fn main() {
-    // Each panel has its own socket. Iterate over them and create a socket
-    let sockets: Vec<UdpSocket> = PORTS
-        .iter()
-        .map(|port| {
-            let socket = UdpSocket::bind(("0.0.0.0", *port)).expect("Failed to bind socket!");
-            socket
-                .connect((IP_ADDRESS, *port))
-                .expect("Failed to connect socket!");
-            socket
-        })
-        .collect();
+    let config_json = 
+    r#"{
+        "offset": {
+            "x": 0,
+            "y": 0
+        },
+        "size": {
+            "x": 64,
+            "y": 32
+        },
+        "panels": [
+            {
+                "socket": "192.168.178.50:26177",
+                "position": {
+                    "x": 0,
+                    "y": 0
+                }
+            },
+            {
+                "socket": "192.168.178.50:26184",
+                "position": {
+                    "x": 64,
+                    "y": 0
+                }
+            },
+            {
+                "socket": "192.168.178.50:26180",
+                "position": {
+                    "x": 0,
+                    "y": 32
+                }
+            },
+            {
+                "socket": "192.168.178.50:26192",
+                "position": {
+                    "x": 64,
+                    "y": 32
+                }
+            }
+        ]
+    }"#;
+
+    let panels = panel::Panels::from_json(config_json).unwrap();
+    #[cfg(debug_assertions)]
+    println!("Deserialized JSON: {:#?}", panels);
+    let mut panel_writer = panel::PanelWriter::new(panels);
 
     let display = scrap::Display::primary().expect("Can't open primary Display");
-
     let mut capturer = scrap::Capturer::new(display).expect("Can't create capturer");
-    let scr_width = capturer.width();
+    let (scr_width, scr_height) = (capturer.width(), capturer.height());
 
-    let mut row_data = Vec::new();
+    // setup for the fps counter
+    let mut frame_duration = std::time::Instant::now();
+    let mut frame_counter = 0;
+    let mut time_acc = 0.0;
 
-    //let now = Instant::now();
     loop {
+        // try to capture a frame
         match capturer.frame() {
             Ok(frame) => {
-                process_frame(frame, &mut row_data, &sockets, scr_width);
+                //show the frame
+                panel_writer.display_image(&frame[..], scr_width, scr_height).expect("Failed to display frame");
+                std::thread::sleep(std::time::Duration::from_millis(TARGET_TIME));
+                
+                // calculate FPS
+                time_acc += frame_duration.elapsed().as_secs_f64();
+                frame_counter += 1;
+                if frame_counter == 50 {
+                    let avg_time = time_acc / (frame_counter as f64);
+                    println!("FPS: {}", 1.0 / avg_time);
+                    frame_counter = 0;
+                    time_acc = 0.0;
+                }
+                frame_duration = std::time::Instant::now();
             }
             Err(ref e) if e.kind() == WouldBlock => {
                 // Wait for the frame.
@@ -51,39 +88,7 @@ fn main() {
                 break;
             }
         }
-    }
-    //println!("{} us", now.elapsed().as_micros());
+    };
 }
 
-fn process_frame(frame: scrap::Frame, row_data: &mut Vec<u8>, sockets: &Vec<UdpSocket>, scr_width: usize) {
-    //let image_data: Vec<&[u8]> = frame.chunks(4).collect();
-    for (socket, offset) in sockets.iter().zip(OFFSETS.iter()) {
-        //update_panel(socket, offset[0], offset[1], capt.get_stored_frame().unwrap(), scr_width as usize);
-        for y in 0..PANEL_HEIGHT {
-            for x in 0..PANEL_WIDTH {
-                let index = ((y + offset[1]) * scr_width + (x + offset[0])) * 4;
-                row_data.extend_from_slice(&pixel_to_packet(
-                    x as u8,
-                    y as u8,
-                    frame[index + 2] / 4,
-                    frame[index + 1] / 4,
-                    frame[index] / 4,
-                ));
-            }
-            socket.send(&row_data).expect("Failed to send data");
-            row_data.clear();
-        }
-    }
 
-    //std::thread::sleep(Duration::from_millis(25));
-}
-
-fn pixel_to_packet(x: u8, y: u8, r: u8, g: u8, b: u8) -> [u8; 4] {
-    let mut data = [0, 0, 0, 0];
-
-    data[0] = y & 0b00111111;
-    data[1] = (x << 2) | (r >> 4);
-    data[2] = (r << 4) | (g >> 2);
-    data[3] = (g << 6) | b;
-    data
-}
---
+++ src/panel.rs
@@ -0,0 +1,108 @@
+use serde::{de::Error, Deserialize, Deserializer};
+use std::net::UdpSocket;
+#[derive(Deserialize, Debug)]
+pub struct Point {
+    x: usize,
+    y: usize,
+}
+#[derive(Deserialize, Debug)]
+pub struct Panels {
+    size: Point,
+    offset: Point,
+    panels: Vec<Panel>,
+}
+#[derive(Deserialize, Debug)]
+pub struct Panel {
+    position: Point,
+    #[serde(deserialize_with = "udp_socket_from_string")]
+    socket: UdpSocket,
+}
+
+pub struct PanelWriter {
+    panels: Panels,
+    row_data: Vec<u8>,
+}
+
+impl Panels {
+    /// Creates the panel Structs from a Json String
+    pub fn from_json(json: &str) -> Result<Self, String> {
+        let deserialized: Self = serde_json::from_str(json).map_err(|e| e.to_string())?;
+
+        Ok(deserialized)
+    }
+}
+
+/// This struct is used to send image data to the LED panel
+impl PanelWriter {
+    pub fn new(panels: Panels) -> Self {
+        let row_data = Vec::new();
+
+        Self { panels, row_data }
+    }
+
+    /// Displays an image in the format BGRA on the LED Panels, with a color Depth of 8 bit per channel, which will be reduced to 6 bit per channel
+    pub fn display_image(
+        &mut self,
+        image_data: &[u8],
+        image_width: usize,
+        _image_height: usize, ) -> Result<(), String> {
+
+        for panel in self.panels.panels.iter() {
+            for y in 0..self.panels.size.y {
+                for x in 0..self.panels.size.x {
+                    let index = 
+                          ((y + panel.position.y + self.panels.offset.y) * image_width
+                        + (x + panel.position.x + self.panels.offset.x))
+                        * 4;
+                    self.row_data.extend_from_slice(&pixel_to_packet(
+                        x as u8,
+                        y as u8,
+                        image_data[index + 2] / 4,
+                        image_data[index + 1] / 4,
+                        image_data[index] / 4,
+                    ));
+                }
+                panel
+                    .socket
+                    .send(&self.row_data)
+                    .map_err(|e| e.to_string())?;
+                self.row_data.clear();
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/// takes a position and a color and creates a 4 byte UDP packet for the LED panel
+fn pixel_to_packet(x: u8, y: u8, r: u8, g: u8, b: u8) -> [u8; 4] {
+    let mut data = [0, 0, 0, 0];
+
+    data[0] = y & 0b00111111;
+    data[1] = (x << 2) | (r >> 4);
+    data[2] = (r << 4) | (g >> 2);
+    data[3] = (g << 6) | b;
+    data
+}
+
+/// creates a UdpSocket from a string in Json
+fn udp_socket_from_string<'de, D>(deserializer: D) -> Result<UdpSocket, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: &str = Deserialize::deserialize(deserializer)?;
+    let split: Vec<&str> = s.split(":").collect();
+    if split.len() != 2 {
+        Err(Error::custom("Wrong format for Socket IP and Port"))
+    } else {
+        let port: u16 = split[1]
+            .parse()
+            .map_err(|_| Error::custom("Failed to parse Port"))?;
+        let socket =
+            UdpSocket::bind(("0.0.0.0", port)).map_err(|_| Error::custom("Failed to bind Port"))?;
+        socket
+            .connect((split[0], port))
+            .map_err(|_| Error::custom("Failed to connect to Socket"))?;
+        Ok(socket)
+    }
+}
